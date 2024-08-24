86700a4ef4481f4fa39ebb1f7527d443460bbea5
Naruebet-s <107977472+bste101@users.noreply.github.com>
2024-08-24 12:33:05 +0700

    fix bug

--- javascript/js/problem2.js
+++ javascript/js/problem2.js
@@ -7,10 +7,9 @@ function readInput() {
         let inputnumber = prompt("Enter an integer (a negative integer to quit):")
         inputnumber = parseInt(inputnumber)
         if (inputnumber >= 0) {
-            array.push(inputnumber) 
+            array.push(inputnumber)
         }
         if (inputnumber < 0) {
-            array.push(0) 
             break
         }
         else {
@@ -21,13 +20,19 @@ function readInput() {
 }
 
 function displayStats(input) {
-    let sum =0
-    for (let i = 0; i < input.length; i++) {
-        sum += input[i]
+    if (input === undefined || input.length == 0) {
+        alert(" For the list that is empty, the average is 0, the minimum is 0, and the maximum is 0.")
     }
-    let avg = sum / input.length
-    avg = avg.toFixed(2)
-    let min = Math.min.apply(Math, input);
-    let max = Math.max.apply(Math, input);
-    alert(" For the list " + input + ", the average is " + avg +", the minimum is " + min +", and the maximum is " + max +".")
+    else {
+        let sum = 0
+        for (let i = 0; i < input.length; i++) {
+            sum += input[i]
+        }
+        let avg = sum / input.length
+        avg = avg.toFixed(2)
+        let min = Math.min.apply(Math, input);
+        let max = Math.max.apply(Math, input);
+        alert(" For the list " + input + ", the average is " + avg + ", the minimum is " + min + ", and the maximum is " + max + ".")
+    }
+
 }
